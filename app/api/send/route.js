dcd98741158abfdfd6917ca82614e27f85e2ad5e
adavidryu <adavidryu@gmail.com>
2024-08-10 22:14:38 -0500

    fix eslint problems

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,37 +1,28 @@
-// pages/api/send.js
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const { email, subject, message } = req.body;
-
-      const data = await resend.emails.send({
-        from: fromEmail,
-        to: [fromEmail, email],
-        subject: subject,
-        react: (
-          <>
-            <h1>{subject}</h1>
-            <p>Thanks for reaching out to me!</p>
-            <p>New message submitted:</p>
-            <p>{message}</p>
-          </>
-        ),
-      });
-
-      return res.status(200).json(data);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: error.message });
-    }
-  } else {
-    return res.status(405).json({ error: 'Method Not Allowed' });
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thanks for reaching out to me!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
   }
 }
-
-export default handler;
