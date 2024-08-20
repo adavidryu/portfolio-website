75884b7f4cdb84f3ea8ad5ed867ad8abe1e03812
adavidryu <adavidryu@gmail.com>
2024-08-20 14:43:20 -0500

    improve responsiveness and fix proj links

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -7,9 +7,9 @@ const projectsData = [
         title: "AI Customer Service Chatbot",
         description: "Next.js, React, OpenAI, Vercel",
         image: "/images/projects/project1.png",
+        gitUrl: "",
+        previewUrl: "http://18.119.129.122/",
         // Add tag ["", ""]
-        // Add gitUrl
-        // Add previewUrl
     },
     {
         id: 2,
@@ -43,7 +43,9 @@ const projectsData = [
         id: 6,
         title: "My Modified Infiniti Q50",
         description: "My pride and joy.",
-        image: "/images/projects/project4.jpg"
+        image: "/images/projects/project4.jpg",
+        gitUrl: "",
+        previewUrl: "",
     }
 ]
 
@@ -54,7 +56,7 @@ const ProjectsSection = () => {
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My Projects
             </h2>
-            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+            <div className="grid lg:grid-cols-3 gap-8 lg:gap-12 md:grid-cols-2">
                 {projectsData.map((project) => (
                     <ProjectCard
                         key={project.id}
